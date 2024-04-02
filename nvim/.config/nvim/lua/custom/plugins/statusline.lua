cfa4df3821306bd856d960fb47e090f14be91998
Micke Larsson <micke@miklar.se>
2024-04-02 22:57:21 +0200

    Only show first letter of mode

--- nvim/.config/nvim/lua/custom/plugins/statusline.lua
+++ nvim/.config/nvim/lua/custom/plugins/statusline.lua
@@ -2,6 +2,17 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup()
+    require('lualine').setup {
+      sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(str)
+              return str:sub(1, 1)
+            end,
+          },
+        },
+      },
+    }
   end,
 }
