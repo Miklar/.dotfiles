2e292a187a2afe34cf2433379e73bb65d4d698c7
Micke Larsson <micke@miklar.se>
2026-01-08 17:39:53 +0100

    use new api for some settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,17 +6,18 @@ vim.g.maplocalleader = "\\"
 
 require("config.lazy")
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.shiftwidth = 4
+-- Indent with 4 spaces
+vim.o.sw = 4
+vim.o.ts = 4
+vim.o.et = true
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- Line numbering
+vim.wo.number = true
+vim.wo.relativenumber = true
+
 vim.opt.termguicolors = true
 
+-- Disable line wrapping
 vim.opt.wrap = false
 
 -- Set borders for all floating windows
