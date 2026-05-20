ccd81c16eeb21cff6f02b8bbe0ad4afc81f1b44e
Micke Larsson <micke@miklar.se>
2026-05-20 11:42:42 +0200

    update brew install list

--- homebrew.sh
+++ homebrew.sh
@@ -8,9 +8,9 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install stow
 brew install git
 
-brew install --cask google-chrome
+#brew install --cask google-chrome
 brew install neovim
-brew install --cask docker
+brew install docker
 #brew install diff-so-fancy #pretty git diffs
 brew install git-delta #pretty git diffs
 
@@ -18,9 +18,18 @@ brew install --cask visual-studio-code
 brew install --cask insomnia
 brew install --cask spotify
 
-brew install todo-txt
 brew install bat
+brew install btop
 brew install eza
+brew install fd
+brew install gh
+brew install jq
+brew install lazydocker
+brew install lazygit
+brew install starship
+brew install tmux
+brew install yazi
+brew install zoxide
 
 brew install nvm
 mkdir ~/.nvm
@@ -32,29 +41,4 @@ brew install --cask font-fira-code
 brew install --cask font-fira-code-nerd-font
 brew install --cask jetbrainsmono-nerd-font
 
-# Zoxide
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
-##brew install fish
-#fish
-#fish_add_path /opt/homebrew/bin
-## set fish as default shell
-#echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
-#chsh -s /opt/homebrew/bin/fish
-#
-##Oh-my-fish
-#curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-##Bass
-#omf install bass
-#
-##Omnisharp
-#curl --verbose --location --remote-name https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.0/omnisharp-osx-x64-net6.0.tar.gz
-#mkdir -p ~/.local/omnisharp
-#mv omnisharp-osx-x64-net6.0.tar.gz ~/.local/omnisharp
-#cd ~/.local/omnisharp
-#tar -xvf omnisharp-osx-x64-net6.0.tar.gz
-#
-##Packer (Neovim plugin manager)
-#git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-# ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-#
